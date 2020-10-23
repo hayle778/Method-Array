30bcb36cc8357c0bc86e4aa477aad1f2b56ad6da
yourUsername <yourEmail@domain.com>
2020-10-23 16:38:23 -0400

    ff

---
+++ src/Method.java
@@ -0,0 +1,30 @@
+
+public class Method {
+
+    public static void main(String[]args){
+        String[] s= {"hey", "sup"};
+       String[] kem = {"Belachew", "Ayele"};
+       int[] bull = {1, 2, 3, 4};
+   // List name of the method to read by the sub method
+        Gel(s);
+    p(kem);
+    f(bull);
+
+    }
+    private static void Gel(String[] s) {
+       System.out.println( s[0] );
+       //    private static void saySomething(int[]mel){
+
+   }
+    private static void p (String[] kem) {
+        System.out.println( kem[0] );
+        //    private static void saySomething(int[]mel){
+
+    }
+
+    private static void f (int[] bull) {
+        System.out.println( bull[0] );
+        //    private static void saySomething(int[]mel){
+    }
+
+}
